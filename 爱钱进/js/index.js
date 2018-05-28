2ea688e2506897e3274bbc5a08d620349949c33c
shiyuemengxiang <24920@163.com>
2018-05-28 22:59:09 +0800

    5月28更新

--- 爱钱进/js/index.js
+++ 爱钱进/js/index.js
@@ -1,5 +1,38 @@
 $(function(){
 	$('#header').load("header.html #header");
     $('#footer').load("header.html #footer");
+    // var width=$(window).width();
+    // // alert(width)
+    // if(width<780){
+    //     window.location="login.html"
+    // }else{
+    //     window.localtion="index.html"
+    // }
+//   轮播图宽度
+			//页面加载执行一次划入banner,出现左右翻页
+         $('#banner').hover(function(){
+         	$('#banner .carousel-control').css('display','block')
+         });
+    $(window).resize(function(){
+        var width=$(window).width();
+        console.log('当前宽度为:'+width+'px');
+        $('#banner').hover(function(){
+            if(width < 1250 ){
+                $('#banner .carousel-control').css({
+                    'display':'none'
+                })
+            }else{
+                $('#banner .carousel-control').css({
+                    'display':'block'
+                })
+            }
+        },function(){
+            $('#banner .carousel-control').css({
+                'display':'none'
+            })
+        })
+    })
+
+
 })
 
---
+++ 爱钱进/js/login.js
@@ -0,0 +1,13 @@
+//注册和登录页个别效果调用(宽度小于980的时候nav和footer隐藏)
+$(function(){
+//	$(window).resize(function(){
+		var width= $(window).width();
+			if(width < 980){
+				var height= $(window).height();
+				$('#content>.container h4').removeClass('pull-right');
+			}else{
+				$('nav .aqj-top,footer').css('display','block');
+				$('.aqj-menu  h4').css({'line-height':'35px'})
+			}
+//	})
+})
